c9ec382893f442423fb16518d3871a09cec26dbd
Derick Campos Santos <164596759+Derickcsantos@users.noreply.github.com>
2024-10-23 20:23:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-document.getElementById('pesquisar').addEventListener('click', function(){
+document.getElementById('pesquisar').addEventListener('click', async function(){
     const cep = document.getElementById('inputCEP').value;
-    // document.getElementById('resultado').innerText = 'Cep: ' + cep;
-    let url = `https://viacep.com.br/ws/${cep}/json/ `;
+    let url = await fetch(`https://viacep.com.br/ws/${cep}/json/ `);
+    const data = await url.json();
+    
+    const uf = data.uf;
+    const cidade = data.localidade;
+    const logradouro = data.logradouro;
 
-    fetch(url)
-                .then(response => response.json())
-                .then(data => alert(JSON.stringify(data)));
-                
+    console.log(`UF: ${uf}`);
+    console.log(`Cidade: ${cidade}`);
+    console.log(`Logradouro: ${logradouro}`);
+    console.log(`CEP: ${cep}`);
+
+    document.getElementById('pUF').innerHTML = `UF: ${uf}`
+    document.getElementById('pLocal').innerHTML = `Cidade: ${cidade}`
+    document.getElementById('pRua').innerHTML = `Rua: ${logradouro}`
+    document.getElementById('pCEP').innerHTML = `CEP: ${cep}`
+    
 })
 
+let popup = document.getElementById('popup')
 
+function iniciar() {
+    
+
+    if(popup){
+        popup.style.display = "block"
+    }else{
+        popup.style.display = "none"
+    }
+};
+
+function fecharPopup() { 
+    if (popup) {
+        popup.style.display = "none";
+    }
+};
